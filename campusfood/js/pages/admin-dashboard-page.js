468c8fd21290a198cb41ab0f14a19001b3c05cf8
bethuelsteven159-rgb <bethuelsteven159@gmail.com>
2026-04-19 13:31:34 +0200

    Add admin role check and render function to dashboard

--- campusfood/js/pages/admin-dashboard-page.js
+++ campusfood/js/pages/admin-dashboard-page.js
@@ -1,1 +1,7 @@
+import { renderAdminName } from '../admin/dashboard.js';
+import { requireRole } from '../shared/guards.js';
 
+export function initAdminDashboardPage() {
+  requireRole('admin');
+  renderAdminName();
+}
