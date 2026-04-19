1e3cb9bd898c60f639c33f47422f7e828349b00c
bethuelsteven159-rgb <bethuelsteven159@gmail.com>
2026-04-19 13:34:42 +0200

    Initialize vendor menu page with necessary imports

--- campusfood/js/pages/vendor-menu-page.js
+++ campusfood/js/pages/vendor-menu-page.js
@@ -1,1 +1,15 @@
+import { renderVendorName } from '../vendor/dashboard.js';
+import { requireRole } from '../shared/guards.js';
+import { loadVendorMenu, addMenuItem, toggleSoldOut, deleteMenuItem } from '../vendor/menu.js';
+import { logout } from '../shared/session.js';
 
+export function initVendorMenuPage() {
+  requireRole('vendor');
+  renderVendorName();
+  loadVendorMenu();
+
+  window.logout = logout;
+  window.addMenuItem = addMenuItem;
+  window.toggleSoldOut = toggleSoldOut;
+  window.deleteMenuItem = deleteMenuItem;
+}
