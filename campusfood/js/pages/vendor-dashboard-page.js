c1e2a5658d2d6a4883c204c16d6fe7ad839bc1ef
bethuelsteven159-rgb <bethuelsteven159@gmail.com>
2026-04-19 13:32:24 +0200

    Add vendor dashboard initialization and role check

--- campusfood/js/pages/vendor-dashboard-page.js
+++ campusfood/js/pages/vendor-dashboard-page.js
@@ -1,1 +1,7 @@
+import { renderVendorName } from '../vendor/dashboard.js';
+import { requireRole } from '../shared/guards.js';
 
+export function initVendorDashboardPage() {
+  requireRole('vendor');
+  renderVendorName();
+}
