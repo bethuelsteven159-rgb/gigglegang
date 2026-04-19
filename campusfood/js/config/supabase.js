b534ec4dfb08c89a3406e441b4407bcc6ba4807c
bethuelsteven159-rgb <bethuelsteven159@gmail.com>
2026-04-19 13:01:28 +0200

    Initialize Supabase client configuration

--- campusfood/js/config/supabase.js
+++ campusfood/js/config/supabase.js
@@ -1,1 +1,4 @@
+const SUPABASE_URL = 'https://mslvqduxmkuusuyaewej.supabase.co';
+const SUPABASE_ANON_KEY = 'YOUR_ANON_KEY_HERE';
 
+export const sb = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
