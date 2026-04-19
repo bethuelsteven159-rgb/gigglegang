33cc53cb0cc9ba286e70fde6950ab62be54940df
bethuelsteven159-rgb <bethuelsteven159@gmail.com>
2026-04-19 13:11:46 +0200

    Add Google sign-in and role-saving functionality

--- campusfood/js/pages/index-page.js
+++ campusfood/js/pages/index-page.js
@@ -1,1 +1,17 @@
+import { signInWithGoogle, checkLoginAfterRedirect } from '../auth/login.js';
+import { saveRoleForFirstTimeUser } from '../auth/role-selection.js';
 
+export function initIndexPage() {
+  const googleBtn = document.getElementById('googleLoginBtn');
+  const saveRoleBtn = document.getElementById('saveRoleBtn');
+
+  if (googleBtn) {
+    googleBtn.addEventListener('click', signInWithGoogle);
+  }
+
+  if (saveRoleBtn) {
+    saveRoleBtn.addEventListener('click', saveRoleForFirstTimeUser);
+  }
+
+  checkLoginAfterRedirect();
+}
