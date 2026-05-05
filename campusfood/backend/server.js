30420b5b02aaa040064f4195f0304da340cdea80
bethuelsteven159-rgb <bethuelsteven159@gmail.com>
2026-05-05 02:19:24 +0200

    Fix server.js to ensure proper PORT declaration

--- campusfood/backend/server.js
+++ campusfood/backend/server.js
@@ -133,7 +133,7 @@ app.get("/api/paystack/verify/:reference", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+ 
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
