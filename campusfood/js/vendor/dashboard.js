96b2f8094b19ff88375b9eb8777c76e5f5cfde92
Bambino09125 <hhmailulah@gmail.com>
2026-05-16 16:22:38 +0200

    Update dashboard.js

--- campusfood/js/vendor/dashboard.js
+++ campusfood/js/vendor/dashboard.js
@@ -1,5 +1,6 @@
 export function renderVendorName() {
   const username = sessionStorage.getItem('username') || 'Vendor';
+
   const name = document.getElementById('vendorName');
   const welcome = document.getElementById('vendorNameWelcome');
 
