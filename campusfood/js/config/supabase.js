7f3521ba9ecfdc7d9cc95d30f295dacd95264b86
Bambino09125 <hhmailulah@gmail.com>
2026-05-18 14:27:39 +0200

    Update supabase.js

--- campusfood/js/config/supabase.js
+++ campusfood/js/config/supabase.js
@@ -1,6 +1,8 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
+import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL     = 'https://mslvqduxmkuusuyaewej.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1zbHZxZHV4bWt1dXN1eWFld2VqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU5ODkzNDcsImV4cCI6MjA5MTU2NTM0N30.VxvR39nI5lNK_JZ6fwctQJgAH06YhbCTd8bXuiLpJgs';
+const SUPABASE_URL = 'YOUR_SUPABASE_URL';
+const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY';
 
 export const sb = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+export const supabase = sb;
