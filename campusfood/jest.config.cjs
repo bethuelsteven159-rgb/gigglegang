8ddbf36ff46bcb750f0303a25c90958452df2f45
Bethuel Kubyana <bethuelsteven159@gmail.com>
2026-05-23 01:27:21 +0200

    Restore backend coverage dependencies

--- campusfood/jest.config.cjs
+++ campusfood/jest.config.cjs
@@ -2,54 +2,53 @@ module.exports = {
   clearMocks: true,
   collectCoverage: true,
 
-  collectCoverageFrom: [
-    '<rootDir>/../backend/**/*.js',
-    '<rootDir>/js/**/*.js',
-
-    // Ignore test files
-    '!**/*.test.js',
-    '!**/*.spec.js',
-
-    // Ignore config/setup files that are tested indirectly or mocked
-    '!<rootDir>/js/config/supabase.js',
-
-    // Ignore files that should not be measured in coverage
-    '!<rootDir>/js/admin_analytics.js',
-    '!<rootDir>/js/main.js',
-
-    // Ignore dependencies
-    '!<rootDir>/node_modules/**',
-    '!<rootDir>/../backend/node_modules/**'
-  ],
-
-  coveragePathIgnorePatterns: [
-    '/node_modules/',
-    '<rootDir>/js/config/supabase.js',
-    '<rootDir>/js/admin_analytics.js',
-    '<rootDir>/js/main.js',
-    '<rootDir>/../backend/node_modules/'
-  ],
-
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov', 'html'],
 
   projects: [
     {
       displayName: 'backend',
+      rootDir: '..',
       testEnvironment: 'node',
       testMatch: [
-        '<rootDir>/../backend/**/*.test.js',
-        '<rootDir>/../backend/**/*.spec.js',
-        '<rootDir>/../backend/server.test.js',
-        '<rootDir>/../backend/**/*.server.test.js'
+        '<rootDir>/backend/**/*.test.js',
+        '<rootDir>/backend/**/*.spec.js'
+      ],
+      collectCoverageFrom: [
+        '<rootDir>/backend/**/*.js',
+        '!<rootDir>/backend/**/*.test.js',
+        '!<rootDir>/backend/**/*.spec.js',
+        '!<rootDir>/backend/node_modules/**'
+      ],
+      coveragePathIgnorePatterns: [
+        '/node_modules/'
       ]
     },
     {
       displayName: 'frontend',
+      rootDir: '.',
       testEnvironment: 'jsdom',
       testMatch: [
         '<rootDir>/js/**/*.test.js',
         '<rootDir>/js/**/*.spec.js'
+      ],
+      collectCoverageFrom: [
+        '<rootDir>/js/**/*.js',
+
+        '!<rootDir>/js/**/*.test.js',
+        '!<rootDir>/js/**/*.spec.js',
+
+        '!<rootDir>/js/config/supabase.js',
+        '!<rootDir>/js/admin_analytics.js',
+        '!<rootDir>/js/main.js',
+
+        '!<rootDir>/node_modules/**'
+      ],
+      coveragePathIgnorePatterns: [
+        '/node_modules/',
+        '<rootDir>/js/config/supabase.js',
+        '<rootDir>/js/admin_analytics.js',
+        '<rootDir>/js/main.js'
       ]
     }
   ]
