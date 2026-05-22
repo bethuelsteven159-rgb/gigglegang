fb5bd212fb489fec24e965f6f46ce1bde4eeef5b
bethuelsteven159-rgb <bethuelsteven159@gmail.com>
2026-05-23 00:45:42 +0200

    Update Jest config for coverage paths and projects

--- campusfood/jest.config.cjs
+++ campusfood/jest.config.cjs
@@ -3,52 +3,54 @@ module.exports = {
   collectCoverage: true,
 
   collectCoverageFrom: [
-    'backend/**/*.js',
-    'js/**/*.js',
+    '<rootDir>/../backend/**/*.js',
+    '<rootDir>/js/**/*.js',
 
     // Ignore test files
     '!**/*.test.js',
     '!**/*.spec.js',
 
     // Ignore config/setup files that are tested indirectly or mocked
-    '!js/config/supabase.js',
+    '!<rootDir>/js/config/supabase.js',
 
     // Ignore files that should not be measured in coverage
-    '!js/admin_analytics.js',
-    '!js/main.js',
+    '!<rootDir>/js/admin_analytics.js',
+    '!<rootDir>/js/main.js',
 
     // Ignore dependencies
-    '!node_modules/**'
+    '!<rootDir>/node_modules/**',
+    '!<rootDir>/../backend/node_modules/**'
   ],
 
   coveragePathIgnorePatterns: [
     '/node_modules/',
     '<rootDir>/js/config/supabase.js',
     '<rootDir>/js/admin_analytics.js',
-    '<rootDir>/js/main.js'
+    '<rootDir>/js/main.js',
+    '<rootDir>/../backend/node_modules/'
   ],
 
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov', 'html'],
 
- projects: [
-  {
-    displayName: 'backend',
-    testEnvironment: 'node',
-    testMatch: [
-      '<rootDir>/../backend/**/*.test.js',
-      '<rootDir>/../backend/**/*.spec.js',
-      '<rootDir>/../backend/server.test.js',
-      '<rootDir>/../backend/**/*.server.test.js'
-    ]
-  },
-  {
-    displayName: 'frontend',
-    testEnvironment: 'jsdom',
-    testMatch: [
-      '<rootDir>/js/**/*.test.js',
-      '<rootDir>/js/**/*.spec.js'
-    ]
-  }
-]
+  projects: [
+    {
+      displayName: 'backend',
+      testEnvironment: 'node',
+      testMatch: [
+        '<rootDir>/../backend/**/*.test.js',
+        '<rootDir>/../backend/**/*.spec.js',
+        '<rootDir>/../backend/server.test.js',
+        '<rootDir>/../backend/**/*.server.test.js'
+      ]
+    },
+    {
+      displayName: 'frontend',
+      testEnvironment: 'jsdom',
+      testMatch: [
+        '<rootDir>/js/**/*.test.js',
+        '<rootDir>/js/**/*.spec.js'
+      ]
+    }
+  ]
 };
